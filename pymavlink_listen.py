6e30c2023b89abcfbf536dc2e450afd0f575abbd
1202martin <martin941079@gmail.com>
2022-06-10 14:07:10 +0900

    finished setting up Ardupilot SITL and QGroundControl; also created simple listener for mavlink heartbeat

---
+++ pymavlink_listen.py
@@ -0,0 +1,9 @@
+from pymavlink import mavutil
+
+#Start a connection listening to a UDP port
+conn = mavutil.mavlink_connection('udpin:localhost:14551')
+
+#Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+conn.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)"%(conn.target_system, conn.target_component))
