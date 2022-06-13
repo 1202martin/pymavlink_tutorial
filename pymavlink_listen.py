b80b87012f80cc8562b4b9d94d2f961cf2cbf265
1202martin <martin941079@gmail.com>
2022-06-13 17:19:22 +0900

    Resolved ardupilot SITL repository issue

--- pymavlink_listen.py
+++ pymavlink_listen.py
@@ -6,4 +6,10 @@ conn = mavutil.mavlink_connection('udpin:localhost:14551')
 #Wait for the first heartbeat
 #   This sets the system and component ID of remote system for the link
 conn.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)"%(conn.target_system, conn.target_component))
+# print("Heartbeat from system (system %u component %u)"%(conn.target_system, conn.target_component))
+
+#Receive all available messages
+while True:
+    msg = conn.recv_match(blocking=True)
+    print("\n", msg)
+    print("type: " ,type(msg))
